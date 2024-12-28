fc6ffdb856d9eebcb16d885ccea24566e4fa2d2e
solana777 <sollysol777@outlook.com>
2024-12-29 02:09:16 +0300

    aaa

---
+++ src/app/(index)/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { BlankView } from 'src/sections/blank/view';
+
+// ----------------------------------------------------------------------
+
+export default function Page() {
+  return <BlankView />;
+}
